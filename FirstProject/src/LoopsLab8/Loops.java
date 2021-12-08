983963769e77314c187e3e6eea90b41c93292dd8
Samih Qureshi <samih.qureshi@gmail.com>
2021-12-07 17:36:27 -0800

    loops.java

--- FirstProject/src/LoopsLab8/Loops.java
+++ FirstProject/src/LoopsLab8/Loops.java
@@ -2,17 +2,18 @@
 /**
  * Unit4.java - Lab for Unit 4 to practice while/for loops
  *
- * @author - Jane Doe
- * @author - Period n
- * @author - Id 0000000
+ * @author - Samih Qureshi
+ * @author - Period 3
+ * @author - Id 10023090
  *
- * @author - I received help from ...
  *
  */
 public class Loops
 {
     public static void main(String[] args)
     {
+
+        // all functions run here
         multiplicationTable(3);
         multiplicationTable(6);
         findPrime(20);
@@ -23,13 +24,10 @@ public class Loops
         checkerBoard(7);
         diamond(3);
         diamond(5);
-
         xmasTree(5);
         xmasTree(7);
-
         birthdayCake(3, 4);
         birthdayCake(5, 3);
-
         verbalArithmetic();
     }
 
@@ -39,7 +37,10 @@ public class Loops
         System.out.println("multiplicationTable(" + n + ")");
 
         int j = 1;
+        // create first column because it never changes
         System.out.println("\tX1\tX2\tX3\tX4\tX5\tX6\tX7\tX8\tX9\tX10");
+
+        // iterates through number of columns (n), multiplies through the table
         while (j <= n) {
                 System.out.print("X" + j);
 
@@ -65,14 +66,14 @@ public class Loops
         int mult2 = 2;
         boolean prime = true;
 
+
+        // iterates through x values, and brute forces values for multiples
+        // if no multiples found then number is prime and it's printed
         while (x <= n) {
-            //System.out.println("trying " + x);
             while(mult1<=n) {
-                //System.out.println("mult1 " + mult1);
                 while(mult2<=n) {
-                    //System.out.println("mult2 " + mult2);
+                    // checks if multiples = x (if true then x is not prime)
                     if(mult1*mult2 == x) {
-                        //System.out.println(x + " not prime");
                         prime = false;
                         break;
                     } else {
@@ -83,6 +84,7 @@ public class Loops
                 mult1++;
                 mult2 = 2;
             }
+            // if prime, then the number is printed, with a space after
             if(prime) {
                 System.out.print(x + " ");
             }
@@ -106,17 +108,19 @@ public class Loops
 
         // Write your implementation below
 
+
+        // for loop starts from the back of the word, puts characters from the back into the front
         for(int x = word.length() - 1; x >= 0; x--) {
             reversedWord += word.charAt(x);
         }
 
+
+        // checks if the reversed word = regular word, prints out accordingly
         if (reversedWord.equals(word)) {
             System.out.println(word + " is a palindrome word.");
         } else {
             System.out.println(word + " is NOT a palindrome word.");
         }
-
-//        System.out.println(reversedWord);
 
     }
 
@@ -128,8 +132,12 @@ public class Loops
 
         // Write your implementation below
 
+
+        // uses row/column format with loops
+        // checks for position of cursor, alternates between * and _
         for(int row = 0; row < n; row++) {
             for(int x = 0; x < n; x++) {
+                // alternates between rows as well so that it looks like a checkerboard
                 if (row % 2 == 0) {
                     if ((x % 2 == 0)) {
                         System.out.print("*");
@@ -165,12 +173,14 @@ public class Loops
 
         for (y = 1; y<=n; y++)
         {
+            // prints spaces before the diamond
             for (z = 1; z<=space; z++) {
                 System.out.print(" ");
             }
 
             space--;
 
+            // prints the stars on one side of the diamond
             for (z = 1; z<= 2*y-1; z++) {
                 System.out.print("*");
             }
@@ -182,12 +192,14 @@ public class Loops
 
         for (y = 1; y<= n - 1; y++)
         {
+            // spaces used again for formatting, when diamond is decreasing in size
             for (z = 1; z<= space; z++) {
                 System.out.print(" ");
             }
 
             space++;
 
+            // star calculation when diamond is decreasing in size
             for (z = 1 ; z<= 2*(n-y)-1; z++) {
                 System.out.print("*");
             }
@@ -214,6 +226,7 @@ public class Loops
 
         for (y = 1; y<=n; y++)
         {
+            // prints the spaces before the tree if necessary
             for (z = 1; z<=space; z++) {
                 System.out.print(" ");
             }
@@ -221,7 +234,9 @@ public class Loops
             space--;
 
             for (z = 1; z<= 2*y-1; z++) {
+                // prints letters, iterates through each one using ++ and --
                 if (z > y) {
+                    // reverses back if past the half way point of tree
                     letter--;
                     System.out.print(letter);
                 } else {
@@ -269,7 +284,7 @@ public class Loops
                 System.out.print(" ");
             }
 
-            //top layer
+            //top layer (of a single layer)
             System.out.print("+");
 
             for (z = 1; z<= radius + (2 * y * radius); z++) {
@@ -280,7 +295,7 @@ public class Loops
 
 
 
-            //middle layer
+            //middle layer (of a single layer)
 
             for (z = 1; z<=space; z++) {
                 System.out.print(" ");
@@ -295,7 +310,7 @@ public class Loops
 
 
 
-            // bottom layer
+            // bottom layer (of a single layer)
 
             for (z = 1; z<=space; z++) {
                 System.out.print(" ");
@@ -308,7 +323,7 @@ public class Loops
 
             System.out.println("+");
 
-
+            // repeats to create the rest of the cake layers
 
         }
 
@@ -337,11 +352,12 @@ public class Loops
 
         char a, b, c, d, e, eOfI;
 
-
+        // iterates between possible answers, divides between single digit numbers
         for(int i = 111111; i <= 999999; i += 111111) {
             for(int divisor = 2; divisor <= 9; divisor++) {
                 int val = i / divisor;
                 if (i % divisor == 0 && val <= 99999) {
+                    // checks to see if the found number fits the requirements
                     stringNumber = String.valueOf(val);
                     eOfI = (String.valueOf(i)).charAt(5);
                     a = stringNumber.charAt(0);
@@ -350,10 +366,12 @@ public class Loops
                     d = stringNumber.charAt(3);
                     e = stringNumber.charAt(4);
 
+                    // following logic makes sure each number is different than each other
                     if (a != b && a != c && a != d && a != e) {
                         if (b != c && b != d && b != e) {
                             if (c != d && c != e) {
                                 if (d != e) {
+                                    // makes sure the E in the multiplication is the same as the E in the answer
                                     if (e == eOfI) {
                                         System.out.println("     " + val);
                                         System.out.println("  X      " + divisor);
@@ -370,6 +388,7 @@ public class Loops
 
             }
 
+            // iterates counter to check how many times looped
                 counter++;
 
         }
